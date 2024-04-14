361dafa565eb809e54e8f1affbae8eebd5059dac
猫苇 <lhyzf@outlook.com>
2024-04-14 06:17:21 +0000

    Update centos7-rust

--- test/centos7-rust/test.sh
+++ test/centos7-rust/test.sh
@@ -5,7 +5,9 @@ source test-utils.sh
 # Template specific tests
 check "distro" cat /etc/redhat-release
 check "rustc" rustc -V
+check "rust-analyzer" rust-analyzer --version
 check "cmake" cmake --version
+check "rust crates index" cat ${CARGO_HOME:-$HOME/.cargo}/config
 
 # Report result
 reportResults
